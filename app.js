95d88e6243ea93665732e73df32a43f37506b4a2
lauratravaglini <72855131+lauratravaglini@users.noreply.github.com>
2022-11-18 09:46:36 +0100

    fix section2 + setting purecounters

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ const textl = document.querySelector("#text-left");
 const textr = document.querySelector("#text-right");
 
 const sectionIntro = document.querySelector("#section-intro");
+
 const sectionOne = document.querySelector("#section1");
+const sectionTwo = document.querySelector("#section2");
+
 const sectionArtwoks = document.querySelector("#artworks1");
 const sectionArtists = document.querySelector("#artists1");
 const sectionAcquisition = document.querySelector("#acquisition1");
@@ -23,12 +26,12 @@ const sections = document.querySelectorAll("section");
 const sectionIntroOptions = {
     root: null,
     threshold: 0.25,
-    rootMargin: "-150px"
+    rootMargin: "-200px"
 };
 
 const sectionIntroObserver = new IntersectionObserver(function(
     entries,
-    sectionOneObserver) {
+    sectionIntroObserver) {
     entries.forEach(entry => {
         if(!entry.isIntersecting) {
             moma.classList.toggle("moma-animated");
@@ -53,26 +56,28 @@ sectionIntroOptions);
 sectionIntroObserver.observe(sectionIntro);
 
 
-const sectionOneOptions = {
+const sectionTwoOptions = {
     root: null,
     threshold: 0.25,
     rootMargin: "-150px"
 };
 
-const sectionOneObserver = new IntersectionObserver(function(
+const sectionTwoObserver = new IntersectionObserver(function(
     entries,
-    sectionOneObserver) {
+    sectionTwoObserver) {
     entries.forEach(entry => {
-        if(!entry.isIntersecting) {
-
+        if(entry.isIntersecting) {
+            moma.classList.toggle("hidden");
+            tate.classList.toggle("hidden");
         } else {
-
+            moma.classList.remove("hidden");
+            tate.classList.remove("hidden");
         }
     });
 }, 
-sectionOneOptions);
+sectionTwoOptions);
   
-sectionOneObserver.observe(sectionOne);
+sectionTwoObserver.observe(sectionTwo);
 
 
 
@@ -98,3 +103,88 @@ const sectionArtwoksObserver = new IntersectionObserver(function(
 sectionArtwoksOptions);
   
 sectionArtwoksObserver.observe(sectionArtwoks);*/
+
+
+// PureCounters 
+
+// PC for visitors
+
+new PureCounter({
+    // Setting that can't' be overriden on pre-element
+    selector: ".pc1", // HTML query selector for spesific element
+  
+    // Settings that can be overridden on per-element basis, by `data-purecounter-*` attributes:
+    start: 0, // Starting number [uint]
+    end: 1160686, // End number [uint]
+    duration: 1, // The time in seconds for the animation to complete [seconds]
+    delay: 10, // The delay between each iteration (the default of 10 will produce 100 fps) [miliseconds]
+    once: true, // Counting at once or recount when the element in view [boolean]
+    pulse: false, // Repeat count for certain time [boolean:false|seconds]
+    decimals: 0, // How many decimal places to show. [uint]
+    legacy: true, // If this is true it will use the scroll event listener on browsers
+    filesizing: false, // This will enable/disable File Size format [boolean]
+    currency: false, // This will enable/disable Currency format. Use it for set the symbol too [boolean|char|string]
+    formater: "us-US", // Number toLocaleString locale/formater, by default is "en-US" [string|boolean:false]
+    separator: false, // This will enable/disable comma separator for thousands. Use it for set the symbol too [boolean|char|string]
+  });
+  
+  new PureCounter({
+    // Setting that can't' be overriden on pre-element
+    selector: ".pc2", // HTML query selector for spesific element
+  
+    // Settings that can be overridden on per-element basis, by `data-purecounter-*` attributes:
+    start: 0, // Starting number [uint]
+    end: 1156037, // End number [uint]
+    duration: 1, // The time in seconds for the animation to complete [seconds]
+    delay: 10, // The delay between each iteration (the default of 10 will produce 100 fps) [miliseconds]
+    once: true, // Counting at once or recount when the element in view [boolean]
+    pulse: false, // Repeat count for certain time [boolean:false|seconds]
+    decimals: 0, // How many decimal places to show. [uint]
+    legacy: true, // If this is true it will use the scroll event listener on browsers
+    filesizing: false, // This will enable/disable File Size format [boolean]
+    currency: false, // This will enable/disable Currency format. Use it for set the symbol too [boolean|char|string]
+    formater: "us-US", // Number toLocaleString locale/formater, by default is "en-US" [string|boolean:false]
+    separator: false, // This will enable/disable comma separator for thousands. Use it for set the symbol too [boolean|char|string]
+  });
+
+  // PC for artworks
+
+  new PureCounter({
+    // Setting that can't' be overriden on pre-element
+    selector: ".pc3", // HTML query selector for spesific element
+  
+    // Settings that can be overridden on per-element basis, by `data-purecounter-*` attributes:
+    start: 0, // Starting number [uint]
+    end: 1160686, // End number [uint]
+    duration: 1, // The time in seconds for the animation to complete [seconds]
+    delay: 10, // The delay between each iteration (the default of 10 will produce 100 fps) [miliseconds]
+    once: true, // Counting at once or recount when the element in view [boolean]
+    pulse: false, // Repeat count for certain time [boolean:false|seconds]
+    decimals: 0, // How many decimal places to show. [uint]
+    legacy: true, // If this is true it will use the scroll event listener on browsers
+    filesizing: false, // This will enable/disable File Size format [boolean]
+    currency: false, // This will enable/disable Currency format. Use it for set the symbol too [boolean|char|string]
+    formater: "us-US", // Number toLocaleString locale/formater, by default is "en-US" [string|boolean:false]
+    separator: false, // This will enable/disable comma separator for thousands. Use it for set the symbol too [boolean|char|string]
+  });
+  
+  new PureCounter({
+    // Setting that can't' be overriden on pre-element
+    selector: ".pc4", // HTML query selector for spesific element
+  
+    // Settings that can be overridden on per-element basis, by `data-purecounter-*` attributes:
+    start: 0, // Starting number [uint]
+    end: 1156037, // End number [uint]
+    duration: 1, // The time in seconds for the animation to complete [seconds]
+    delay: 10, // The delay between each iteration (the default of 10 will produce 100 fps) [miliseconds]
+    once: true, // Counting at once or recount when the element in view [boolean]
+    pulse: false, // Repeat count for certain time [boolean:false|seconds]
+    decimals: 0, // How many decimal places to show. [uint]
+    legacy: true, // If this is true it will use the scroll event listener on browsers
+    filesizing: false, // This will enable/disable File Size format [boolean]
+    currency: false, // This will enable/disable Currency format. Use it for set the symbol too [boolean|char|string]
+    formater: "us-US", // Number toLocaleString locale/formater, by default is "en-US" [string|boolean:false]
+    separator: false, // This will enable/disable comma separator for thousands. Use it for set the symbol too [boolean|char|string]
+  });
+  
+// End PureCounters  
