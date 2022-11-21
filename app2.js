0adc44a66e552931f9a24ee278e7f3d74a93842d
lauratravaglini <72855131+lauratravaglini@users.noreply.github.com>
2022-11-21 14:36:38 +0100

    animation basic

--- app2.js
+++ app2.js
@@ -81,30 +81,35 @@ new PureCounter({
 
 // Observer
 
-/*
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
 
-const sections = document.querySelectorAll("section");
-
-const sectionsOptions = {
-    root: null,
-    threshold: 0.25
-};
-
-const sectionsObserver = new IntersectionObserver(function(
-    entries,
-    sectionsObserver) {
-    entries.forEach(entry => {
-        if(entry.isIntersecting) {
-            sections.classList.toggle("show");
-            
-        } else {
-            sections.classList.remove("show");
-        }
-    });
-}, 
-sectionsOptions);
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -250px 0px"
+  };
   
-sectionsObserver.observe(sections);
+  const appearOnScroll = new IntersectionObserver(function(
+    entries,
+    appearOnScroll
+  ) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  },
+  appearOptions);
+  
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
+  
+  sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+  });
 
-*/
 
